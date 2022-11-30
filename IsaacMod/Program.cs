5b79128bed819532834276d5f9542f1abadd1f80
a név minek <usscommando@gmail.com>
2022-11-30 14:50:02 +0100

    Add project files.

---
+++ IsaacMod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IsaacMod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("stringtable.sta"))
+            {
+                int j = 1348;
+                for (int i = 0; i < 1348; i++)
+                {
+                    sr.ReadLine();
+                }
+                while (!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine());
+                    for (int i = 0; i < 8; i++)
+                    {
+                        sr.ReadLine();
+                    }
+                    Console.WriteLine(sr.ReadLine());
+                    sr.ReadLine();
+                    j++;
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
